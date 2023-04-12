68193bc668553c0ff538e286b23cf40217e4e2b1
HuongNV <huongnv@telsoft.com.vn>
2023-04-12 10:04:27 +0700

    init project

---
+++ src/main/java/com/telsoft/github_cicd_jenkin_docker/GithubCicdJenkinDockerApplication.java
@@ -0,0 +1,13 @@
+package com.telsoft.github_cicd_jenkin_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdJenkinDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdJenkinDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telsoft/github_cicd_jenkin_docker/GithubCicdJenkinDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telsoft.github_cicd_jenkin_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdJenkinDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
